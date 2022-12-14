8ea27b19102e34910debf0472cac59461f9085fb
suryapulicharla8 <suryapulicharla8@gmail.com>
2022-12-14 14:28:01 +0530

    functions

---
+++ main.js
@@ -0,0 +1,59 @@
+// q.1
+
+function surya(x,y){
+    console.log(surya)
+}
+
+// Q 2.
+function add(a,b){
+    console.log(a+b)
+}
+add(3,7)
+
+// Q3
+let myFunction =(a,b)=>{
+    console.log(a*b)
+}
+myFunction (4,5)
+
+// Q4 
+        var x = 21;
+        var girl = function () {
+            console.log(x);
+            var x = 20;
+        };
+        girl ();
+
+// Q5
+        var x = 21;
+        girl ();
+        console.log(x)
+        function girl() {
+            console.log(x);
+            var x = 20;
+        };
+
+// Q6 
+var x = 21;
+        a();
+        b();
+        
+          function a() {
+            
+           x = 20;
+          console.log(x);
+        };
+         function b() {
+            
+            x = 40;
+           console.log(x);
+        };
+
+
+    // Q7
+    var n=1234;
+    function myFunction1(n){
+        console.log(n)
+    }
+    myFunction1=n
+
